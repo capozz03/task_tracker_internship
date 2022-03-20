8b76c6fa57d117a496d3d290ee4dacc14b45b2fe
Erichev Egor <ErichevEA@lad24.ru>
2022-03-20 21:14:36 +0300

    исправить конфликты слияния

---
+++ src/features/Task/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const TaskPageComponent = () => <h1>Task Page</h1>;
---
+++ src/pages/TaskPage/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { TaskPageComponent } from 'features';
+
+export const TaskPage = () => <TaskPageComponent />;
