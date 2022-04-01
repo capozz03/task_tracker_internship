8dc693e7577ab320d4779fa46968b37b8e5ac5f8
Дмитрий Куликов <demonpk3@gmail.com>
2022-04-01 16:14:13 +0300

    Установка русской локализации для moment перенесена в app/App.tsx, добавлен компонент DateWithIconClock.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { AppRouter } from './routing/AppRouter';
 import 'shared/assets/styles/index.scss';
+import moment from 'moment';
+import 'moment/locale/ru';
+
+moment().locale('ru');
 
 const App = () => <AppRouter />;
 
--- src/features/Tasks/tasksComponents/Date/index.tsx
+++ src/features/Tasks/tasksComponents/Date/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import moment from 'moment';
-import 'moment/locale/ru';
 
 type DateProps = {
   date: string
 }
 
 const Date = ({ date }: DateProps) => {
-  moment().locale('ru');
   const dates = moment(date).format('DD MMM YYYY').replace('.', '');
   return (
     <span>{ dates }</span>
