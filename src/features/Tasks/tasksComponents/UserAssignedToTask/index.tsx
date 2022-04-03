11b48950bcb2a65055981e88d57e44f9a7d9d19e
Дмитрий Куликов <demonpk3@gmail.com>
2022-04-03 13:38:21 +0300

    Компонент UserAssignedToTask показывает аватарки пользователей только тех которые убираются по ширине, все остальные заменяются на "+количество скрытых пользователей"

--- src/features/Tasks/tasksComponents/UserAssignedToTask/index.tsx
+++ src/features/Tasks/tasksComponents/UserAssignedToTask/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import UserAvatar from 'features/Tasks/tasksComponents/UserAvatar';
 import classes from './index.module.scss';
 
@@ -6,11 +6,48 @@ type UserAssignedToTaskProps = {
 users: any[]
 }
 
-const UserAssignedToTask = ({ users }: UserAssignedToTaskProps) => (
-  <div className={classes.users}>
-    {users.map((user) => (
-      <UserAvatar user={user} />
-    ))}
-  </div>);
+type OtherProps = {
+  count: number
+}
+
+// Вынес только потому что ESLint ругается
+const Other = ({ count }: OtherProps) => (
+  <div className={classes.others}>
+    {`+${count}`}
+  </div>
+);
+
+const UserAssignedToTask = ({ users }: UserAssignedToTaskProps) => {
+  const wrap = useRef(null);
+  const [countElement, setCountElement] = useState(0);
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    const resizeFN = (e: Event) => {
+      // @ts-ignore
+      setWidth(e.target.innerWidth);
+    };
+    window.addEventListener('resize', resizeFN);
+    return () => {
+      window.removeEventListener('resize', resizeFN);
+    };
+  }, []);
+  useEffect(() => {
+    // TODO: Как исправить?
+    // @ts-ignore
+    setCountElement(Math.floor(wrap.current.offsetWidth / 42) - 1);
+  }, [wrap.current, width]);
+  return (
+    <div className={classes.users} ref={wrap}>
+      {users.slice(0, countElement).map((user) => (
+        <UserAvatar key={user.user_id} user={user} />
+      ))}
+      {
+        users.length - countElement > 0
+          ? <Other count={users.length - countElement} />
+          : ''
+      }
+
+    </div>);
+};
 
 export default UserAssignedToTask;
