bda2eca8f3d2adbefd1d222fd217f47b99112414
Smoke52ru <Smoke52ru@ya.ru>
2022-04-04 19:30:39 +0300

    revert main page changes

--- src/features/Tasks/index.tsx
+++ src/features/Tasks/index.tsx
@@ -1,22 +1,3 @@
 import React from 'react';
-import CardName from './tasksComponents/CardName';
 
-const props = {
-  name: 'Какая-то задача',
-  attachments: 5,
-  checkListTotal: 9,
-  checkListChecked: 4,
-};
-const moreProps = {
-  name: 'Какая-то задача, но чуть подлиннее, совсем чуть-чуть',
-  attachments: 55,
-  checkListTotal: 99,
-  checkListChecked: 44,
-};
-
-export const TasksPageComponent = () => (
-  <div style={{ maxWidth: '300px' }}>
-    <CardName {...props} />
-    <CardName {...moreProps} />
-  </div>
-);
+export const TasksPageComponent = () => <h1>Tasks Page</h1>;
