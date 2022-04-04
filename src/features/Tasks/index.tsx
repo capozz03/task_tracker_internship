30968d83b511ae53f6b3a7afae6f66df9dd3d291
Smoke52ru <Smoke52ru@ya.ru>
2022-04-04 19:32:10 +0300

    revert main page changes

--- src/features/Tasks/index.tsx
+++ src/features/Tasks/index.tsx
@@ -1,9 +1,3 @@
 import React from 'react';
-import Button from './tasksComponents/Button';
 
-export const TasksPageComponent = () => (
-  <span style={{ display: 'flex', height: '100px' }}>
-    <Button type="primary">Кнопа</Button>
-    <Button type="default"> Неважная кнопа</Button>
-  </span>
-);
+export const TasksPageComponent = () => <h1>Tasks Page</h1>;
