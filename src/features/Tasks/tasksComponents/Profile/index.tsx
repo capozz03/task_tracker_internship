3908d1ca012b656735488c4e1c65c524641b1bb6
Smoke52ru <Smoke52ru@ya.ru>
2022-04-06 04:03:11 +0300

    Заготовка с иконкой и меню профиля

--- src/features/Tasks/tasksComponents/Profile/index.tsx
+++ src/features/Tasks/tasksComponents/Profile/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import styles from '../../TasksLayout/index.module.scss';
+import styles from './index.module.scss';
 import { Avatar, Dropdown } from 'antd';
 import { CaretDownOutlined, UserOutlined } from '@ant-design/icons';
 import { menu } from './ProfileDropdownMenu';
 
-const Profile = () => (
-  <div className={styles.profile}>
-    <Avatar size={34} icon={<UserOutlined />} />
+// @ts-ignore
+// eslint-disable-next-line react/prop-types
+const Profile = ({ className }) => (
+  <div className={className}>
+    <Avatar size={34} icon={<UserOutlined />} className={styles.profileAvatar} />
     <Dropdown overlay={menu} trigger={['click']}>
       <CaretDownOutlined />
     </Dropdown>
