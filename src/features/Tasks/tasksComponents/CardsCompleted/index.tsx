5ac2d95d28899fa125cd0e952fbb647d03447aea
capozz <mr.vitker@gmail.com>
2022-04-07 22:34:23 +0300

    добавил адаптив под мобилку, хук useBreakPoint

--- src/features/Tasks/tasksComponents/CardsCompleted/index.tsx
+++ src/features/Tasks/tasksComponents/CardsCompleted/index.tsx
@@ -1,22 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useBreakPoint } from 'shared/helpers/hooks/useBreakPoint';
 import { SortByMobileScreen, SortByPCScreen } from '../SortBy';
 import { CardCompleted } from './CardCompleted';
 import { tags, user } from './data';
 import style from './index.module.scss';
 
 export const CardsCompleted = () => {
-  const [windowWidth, setWindowWidth] = useState<number>(0);
-
-  useEffect(() => {
-    setWindowWidth(window.innerWidth);
-  }, []);
+  const isMobile = useBreakPoint(768);
 
   return (
     <div className={style.cardCompleted}>
       <div className={style.container}>
         <div className={style.cardCompletedTitle}>
           <div className={style.title}>Завершено</div>
-          {windowWidth >= 768 ? <SortByPCScreen /> : <SortByMobileScreen />}
+          {isMobile ? <SortByMobileScreen /> : <SortByPCScreen />}
         </div>
         <CardCompleted user={user} tags={tags} />
       </div>
