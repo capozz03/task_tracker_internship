b8133c5a52b6576e16c4e300463f6431e9c0a7f5
Erichev Egor <ErichevEA@lad24.ru>
2022-04-07 18:48:59 +0300

    исправить ошибки сборки

--- src/features/Tasks/tasksComponents/CardsCompleted/data.ts
+++ src/features/Tasks/tasksComponents/CardsCompleted/data.ts
@@ -3,10 +3,4 @@ export const user = {
   name: 'Евгения Горелова',
   logo: 'https://i03.fotocdn.net/s121/f6dbed805aaf6dfa/user_l/2777981224.jpg',
 };
-export const tags = [
-  {
-    task_tag_id: 'd1403ea9-aba2-4c03-a2ff-3bfe9385d38f',
-    name: 'Срочно',
-    color: '#FC5A5A',
-  },
-];
+export const tags = [];
