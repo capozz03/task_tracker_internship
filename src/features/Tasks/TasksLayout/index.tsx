d530c3ac338963337db368328ff16aaccad3a365
Smoke52ru <Smoke52ru@ya.ru>
2022-04-07 05:04:56 +0300

    Адаптив на широкий и средний экран вроде готов

--- src/features/Tasks/TasksLayout/index.tsx
+++ src/features/Tasks/TasksLayout/index.tsx
@@ -1,10 +1,11 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.scss';
 import { Layout } from 'antd';
 import FilterAssignedTo from '../tasksComponents/FilterAssignedTo';
 import SidebarSearchInput from '../tasksComponents/SidebarSearchInput';
 import Profile from '../tasksComponents/Profile';
+import NotificationsButton from '../tasksComponents/NotificationsButton';
+import FilterToggleButton from '../tasksComponents/FilterToggleButton';
 
 const { Sider, Header, Content } = Layout;
 
@@ -12,22 +13,33 @@ type TasksLayoutProps = {
   children: React.ReactNode;
 };
 
-const TasksLayout = ({ children }: TasksLayoutProps) => (
-  <Layout className={styles.layout}>
-    <Sider className={styles.sider}>
-      <div className={styles.wrapper}>
-        <SidebarSearchInput placeholder="Поиск" />
-      </div>
-    </Sider>
-    <Layout className={styles.main}>
-      <Profile className={styles.profile} />
-      <Header className={styles.header}>
-        <div className={styles.text}>Задачи</div>
-        <FilterAssignedTo currentValue="all" />
-      </Header>
-      <Content className={styles.content}>{children}</Content>
+const TasksLayout = ({ children }: TasksLayoutProps) => {
+  const [sidebarShow, setSidebarShow] = useState(false);
+
+  return (
+    <Layout className={styles.layout}>
+      <Sider className={styles.sider} collapsed={!sidebarShow} collapsedWidth={0}>
+        <div className={styles.wrapper}>
+          <SidebarSearchInput placeholder="Поиск" />
+        </div>
+      </Sider>
+      <Layout className={styles.main}>
+        <Header className={styles.header}>
+          <div className={styles.text}>Задачи</div>
+          <Profile className={styles.profile} />
+          <div className={styles.tools}>
+            <FilterToggleButton
+              active={false}
+              onClick={() => setSidebarShow(!sidebarShow)}
+            />
+            <NotificationsButton active={false} />
+          </div>
+          <FilterAssignedTo currentValue="all" />
+        </Header>
+        <Content className={styles.content}>{children}</Content>
+      </Layout>
     </Layout>
-  </Layout>
-);
+  );
+};
 
 export default TasksLayout;
