5196b32432c93a02461cbe274c668aa8a384df54
Erichev Egor <ErichevEA@lad24.ru>
2022-04-11 20:19:36 +0300

    исправить ошибку типа

--- src/features/Tasks/tasksComponents/NewTask/InputNameTask/index.tsx
+++ src/features/Tasks/tasksComponents/NewTask/InputNameTask/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './index.module.scss';
 
 const InputNameTask = ({ ...props }: React.HTMLProps<HTMLInputElement>) => {
-
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
   };
