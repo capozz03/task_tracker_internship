4382319ad7da994e185610fb2571cbbda0d0fe4a
Smoke52ru <Smoke52ru@ya.ru>
2022-04-12 11:57:43 +0300

    сделал enum со статусами таски

--- src/shared/helpers/enums.ts
+++ src/shared/helpers/enums.ts
@@ -5,3 +5,11 @@ export enum RequestStatuses {
   SUCCESS = 'SUCCESS',
   FAILURE = 'FAILURE',
 }
+
+export enum TaskStatuses {
+  CREATED = 'cbb7199e-cb25-4dce-bf4e-24a8a5e07ef2',
+  IN_WORK = '372d63ff-3ae3-4be2-a606-38940d7f8c8f',
+  COMPLETED = '8536592a-7340-4e10-ac4b-a280652c9310',
+  FAILED = '599f5d03-1ef0-4a5b-a18c-33a4f44c4610',
+  REJECTED = '4658859a-32a6-4206-838a-c0064f147299',
+}
