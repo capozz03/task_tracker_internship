a424fe36841e94a8521caa359eb7ab36ce63c28f
Smoke52ru <Smoke52ru@ya.ru>
2022-04-12 11:05:15 +0300

    Убрал компоненты входящих, в работе и завершенных из-за циклических импортов

--- src/features/Tasks/tasksComponents/index.ts
+++ src/features/Tasks/tasksComponents/index.ts
@@ -1,26 +1,23 @@
 import Button from './Button';
+import CardChecklistCount from './CardChecklistCount';
+import CardAttachmentsCount from './CardAttachmentsCount';
 import CardName from './CardName';
+import CardNameText from './CardNameText';
 import DateWithIconClock from './DateWithIconClock';
 import DropdownMenu from './DropdownMenu';
 import FilterAssignedTo from './FilterAssignedTo';
-import NewTask from './NewTask';
 import FilterToggleButton from './FilterToggleButton';
+import NewTask from './NewTask';
 import NotificationsButton from './NotificationsButton';
-import PriorityStatus from './PriorityStatus';
-import SidebarSearchInput from './SidebarSearchInput';
 import Pagination from './Pagination';
-import { SortByMobileScreen, SortByPCScreen } from './SortBy';
-import TagsGroup from './TagsGroup';
-import TasksInbox from './TasksInbox';
-import TasksInWork from './TasksInWork';
+import SidebarSearchInput from './SidebarSearchInput';
+import { SortByPCScreen, SortByMobileScreen } from './SortBy';
+import PriorityStatus from './PriorityStatus';
 import Tag from './Tag';
+import TagsGroup from './TagsGroup';
+import TaskStatus from './TaskStatus';
 import UserAssignedToTask from './UserAssignedToTask';
 import UserAvatar from './UserAvatar';
-import TaskStatus from './TaskStatus';
-import CardAttachmentsCount from './CardAttachmentsCount';
-import CardChecklistCount from './CardChecklistCount';
-import CardNameText from './CardNameText';
-import TasksCompleted from './TasksCompleted';
 
 export {
   Button,
@@ -41,9 +38,6 @@ export {
   SortByMobileScreen,
   Tag,
   TagsGroup,
-  TasksCompleted,
-  TasksInbox,
-  TasksInWork,
   TaskStatus,
   UserAssignedToTask,
   UserAvatar,
