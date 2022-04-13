7c4b6cc34c412555aa6f0cdbc0a1105779b30457
Your Name <you@example.com>
2022-04-13 13:11:03 +0300

    начал стилизацию тоста

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { MainSlice } from 'store/slice';
 
+// must remove
+import Alert from 'shared/ui/icons/Alert';
+
 export const MainPage = () => {
   const dispatch = useDispatch();
 
@@ -9,5 +12,10 @@ export const MainPage = () => {
     dispatch(MainSlice.addTodo({ id: 4, title: 'TODO 2' }));
   }, []);
 
-  return <h1>MainPage</h1>;
+  return (
+    <div>
+      <h1>MainPage</h1>
+      <Alert text="props text efgSE" />
+    </div>
+  );
 };
