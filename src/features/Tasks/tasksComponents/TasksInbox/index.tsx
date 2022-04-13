c451236375f287c5ba87c407f451dd170e876cc3
Smoke52ru <Smoke52ru@ya.ru>
2022-04-13 10:38:11 +0300

    UI новой задачи и сортировки

--- src/features/Tasks/tasksComponents/TasksInbox/index.tsx
+++ src/features/Tasks/tasksComponents/TasksInbox/index.tsx
@@ -1,17 +1,26 @@
-import React, { ComponentProps, useEffect } from 'react';
+import React, { ComponentProps, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { TaskInboxSlice } from 'store/slice';
 import styles from './index.module.scss';
 import TaskInbox from './TaskInbox';
 import Pagination from '../Pagination';
+import { useBreakPoint } from 'shared/helpers/hooks/useBreakPoint';
+import { NewTask, SortByMobileScreen, SortByPCScreen } from '..';
+import { TaskStatuses } from 'shared/helpers/enums';
+
+type TSortType = 'date~DESC' | 'title~ASC';
 
 const TasksInbox = (props: ComponentProps<any>) => {
+  const isMobile = useBreakPoint(768);
   const dispatch = useDispatch();
   const pagination = useSelector(TaskInboxSlice.getPagination);
   const tasks = useSelector(TaskInboxSlice.getTasks);
+  const [sortType, setSortType] = useState<TSortType>('date~DESC');
+
   const paginationHandler = (page: number, pageSize: number) => {
     dispatch(
       TaskInboxSlice.getTasksAsync({
+        sort: sortType,
         page,
         per_page: pageSize,
       }),
@@ -21,6 +30,7 @@ const TasksInbox = (props: ComponentProps<any>) => {
   useEffect(() => {
     dispatch(
       TaskInboxSlice.getTasksAsync({
+        sort: sortType,
         page: 1,
         per_page: 3,
       }),
@@ -29,15 +39,29 @@ const TasksInbox = (props: ComponentProps<any>) => {
 
   return (
     <div className={styles.tasks_group} {...props}>
-      <h4 className={styles.title}>Входящие</h4>
+      <div className={styles.header}>
+        <h4 className={styles.title}>Входящие</h4>
+        <div className={styles.sort}>
+          {isMobile ? (
+            <SortByMobileScreen setSortType={setSortType} />
+          ) : (
+            <SortByPCScreen setSortType={setSortType} />
+          )}
+        </div>
+      </div>
       {tasks && tasks.map((task) => <TaskInbox key={task.task_id} task={task} />)}
-      <div className={styles.pagination}>
-        {pagination && (
-          <Pagination
-            onChange={paginationHandler}
-            total={pagination.page_total * pagination.per_page}
-          />
-        )}
+      <div className={styles.footer}>
+        <div className={styles.createTask}>
+          <NewTask taskStatusId={TaskStatuses.CREATED} />
+        </div>
+        <div className={styles.pagination}>
+          {pagination && (
+            <Pagination
+              onChange={paginationHandler}
+              total={pagination.page_total * pagination.per_page}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
