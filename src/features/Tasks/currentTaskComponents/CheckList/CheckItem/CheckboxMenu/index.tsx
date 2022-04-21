a20ee17c85dd5d3fe71a1626e6da2dd3378c6b9e
Дмитрий Куликов <demonpk3@gmail.com>
2022-04-21 18:32:32 +0300

    Исправлено меню

--- src/features/Tasks/currentTaskComponents/CheckList/CheckItem/CheckboxMenu/index.tsx
+++ src/features/Tasks/currentTaskComponents/CheckList/CheckItem/CheckboxMenu/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Dropdown, Menu } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import styles from './index.module.scss';
 import { TaskFormSlice } from 'store/slice';
 import { EllipsisOutlined } from '@ant-design/icons';
@@ -11,40 +11,34 @@ type CheckboxMenuProps = {
   editItem: () => void,
 }
 
+const { Item } = Menu;
+
+const OptionMenu = ({ editItem, deleteItem }: any) => (
+  <Menu className={styles.dropdownMenu}>
+    <Item key="1" onClick={editItem}>
+      Редактировать
+    </Item>
+    <Item key="2" onClick={deleteItem} danger>
+      Удалить
+    </Item>
+  </Menu>
+);
+
 const CheckboxMenu = ({ checkListItemId, checkListId, editItem }: CheckboxMenuProps) => {
-  const { Item } = Menu;
-  const task = useSelector(TaskFormSlice.getTask);
   const dispatch = useDispatch();
-  if (task) {
-    const deleteItem = () => {
-      dispatch(TaskFormSlice.deleteItemForChecklist({
-        checkListItemId,
-        checkListId,
-      }));
-    };
-
-    const menu = (
-      <Menu className={styles.dropdownMenu}>
-        <Item key="1" onClick={editItem}>
-          Редактировать
-        </Item>
-        <Item key="2" onClick={deleteItem} danger>
-          Удалить
-        </Item>
-      </Menu>
-    );
-
-    return (
-      <Dropdown.Button
-        className={styles.dropdownButton}
-        overlay={menu}
-        trigger={['click']}
-        icon={<EllipsisOutlined className={styles.dropdownIcon} />}
-      />
-    );
-  }
+  const deleteItem = () => {
+    dispatch(TaskFormSlice.deleteItemForChecklist({
+      checkListItemId,
+      checkListId,
+    }));
+  };
   return (
-    <div>Ошибка, задача не выбрана</div>
+    <Dropdown.Button
+      className={styles.dropdownButton}
+      overlay={<OptionMenu editItem={editItem} deleteItem={deleteItem} />}
+      trigger={['click']}
+      icon={<EllipsisOutlined className={styles.dropdownIcon} />}
+    />
   );
 };
 
