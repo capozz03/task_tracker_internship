6179371b0cfcaf4ca44170eb4a577bda5c15ac67
Дмитрий Куликов <demonpk3@gmail.com>
2022-04-21 13:10:11 +0300

    Обновление списков задач, после закрытия Модального окна с задачей

--- src/features/Tasks/currentTaskComponents/TaskModal/index.tsx
+++ src/features/Tasks/currentTaskComponents/TaskModal/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Modal, ModalProps, Spin } from 'antd';
 import styles from './index.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { TaskFormSlice } from 'store/slice';
+import { TaskFormSlice, TaskInWorkSlice, TaskInboxSlice, TaskCompletedSlice } from 'store/slice';
 import { Title } from 'features/Tasks/currentTaskComponents';
 import MenuHeader from 'features/Tasks/currentTaskComponents/MenuHeader';
 import CheckListArea from 'features/Tasks/currentTaskComponents/CheckListArea';
@@ -11,7 +11,26 @@ const TaskModal = (props: ModalProps) => {
   const dispatch = useDispatch();
   const task = useSelector(TaskFormSlice.getTask);
   const isLoading = useSelector(TaskFormSlice.isLoadingStatus);
+  const paginationInbox = useSelector(TaskInboxSlice.getPagination);
+  const paginationInWork = useSelector(TaskInWorkSlice.getPagination);
+  const paginationInCompleted = useSelector(TaskCompletedSlice.getPagination);
+
   const cancelHandle = () => {
+    if (task?.status.name === 'Создана') {
+      dispatch(TaskInboxSlice.getTasksAsync({
+        per_page: paginationInbox!.per_page,
+        page: paginationInbox!.page_current }));
+    }
+    if (task?.status.name === 'В работе') {
+      dispatch(TaskInWorkSlice.getTasksAsync({
+        per_page: paginationInWork!.per_page,
+        page: paginationInWork!.page_current }));
+    }
+    if (task?.status.name === 'Выполнена' || task!.status.name === 'Не выполнена') {
+      dispatch(TaskCompletedSlice.getTasksAsync({
+        per_page: paginationInCompleted!.per_page,
+        page: paginationInCompleted!.page_current }));
+    }
     dispatch(TaskFormSlice.hiddenTaskForm());
   };
   return (
