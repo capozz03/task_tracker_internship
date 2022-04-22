4037bd60c3a28e0459999984c22f844c3c27233a
Дмитрий Куликов <demonpk3@gmail.com>
2022-04-22 15:33:20 +0300

    fix drop menu checkboxItem

--- src/features/Tasks/currentTaskComponents/MenuHeader/AttachMenu/index.tsx
+++ src/features/Tasks/currentTaskComponents/MenuHeader/AttachMenu/index.tsx
@@ -4,41 +4,41 @@ import { useDispatch, useSelector } from 'react-redux';
 import styles from './index.module.scss';
 import { TaskFormSlice } from 'store/slice';
 import PlusIcons from 'shared/ui/icons/PlusIcons';
+import { alert } from 'shared';
 
 const AttachMenu = () => {
   const dispatch = useDispatch();
   const { Item } = Menu;
-  const task = useSelector(TaskFormSlice.getTask);
-  if (task) {
-    const checklistHandle = () => {
+  const checklists = useSelector(TaskFormSlice.getCheckLists);
+  const checklistHandle = () => {
+    if (checklists && checklists.length >= 3) {
+      alert('В задаче не может быть более 3-х чеклистов', 'error');
+    } else {
       dispatch(TaskFormSlice.showFormCreateChecklist());
-    };
-    const attachFileHandle = () => {
+    }
+  };
+  const attachFileHandle = () => {
 
-    };
+  };
 
-    const menu = (
-      <Menu className={styles.dropdownMenu}>
-        <Item key="1" onClick={checklistHandle}>
-          Добавить чек-лист
-        </Item>
-        <Item key="2" onClick={attachFileHandle}>
-          Прикрепить вложение
-        </Item>
-      </Menu>
-    );
+  const menu = (
+    <Menu className={styles.dropdownMenu}>
+      <Item key="1" onClick={checklistHandle}>
+        Добавить чек-лист
+      </Item>
+      <Item key="2" onClick={attachFileHandle}>
+        Прикрепить вложение
+      </Item>
+    </Menu>
+  );
 
-    return (
-      <Dropdown.Button
-        className={styles.dropdownButton}
-        overlay={menu}
-        trigger={['click']}
-        icon={<PlusIcons />}
-      />
-    );
-  }
   return (
-    <div>Ошибка, задача не выбрана</div>
+    <Dropdown.Button
+      className={styles.dropdownButton}
+      overlay={menu}
+      trigger={['click']}
+      icon={<PlusIcons />}
+    />
   );
 };
 
