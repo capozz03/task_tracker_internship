24b31bb892d4463c917ef692aba2383455f9fffc
Your Name <you@example.com>
2022-05-11 18:42:26 +0300

    начал работу над статусом

--- src/features/Task/taskModalComponents/Details/index.tsx
+++ src/features/Task/taskModalComponents/Details/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RolesIds } from 'shared';
 import { isAuthor, isResponsible } from 'shared/helpers';
 import { TaskFormSlice, UserSlice } from 'store/slice';
@@ -9,10 +9,13 @@ import MembersChanger from '../MembersChanger';
 import UserLabel from '../UserLabel';
 import styles from './index.module.scss';
 import { TaskStatus } from 'features/Tasks/tasksComponents';
+import { TaskStatuses } from 'shared/helpers/enums';
 
 const Details = () => {
+  const dispatch = useDispatch();
   const roles = useSelector(TaskFormSlice.getRoles);
   const status = useSelector(TaskFormSlice.getTaskFormStatusTask);
+  const currentTaskId = useSelector(TaskFormSlice.getTaskFormId);
   const currentUserId = useSelector(UserSlice.userId);
 
   const isAuthorOrResponsible = isAuthor(currentUserId, roles)
@@ -23,9 +26,13 @@ const Details = () => {
     return <p className={styles.notPerformer}>Без исполнителя</p>;
   };
 
-  useEffect(() => {
-    console.log(status);
-  }, [status]);
+  const statusChanger = (prevStatusId: string, taskId: string, newStatusId: string) => {
+    console.log(prevStatusId, taskId, newStatusId);
+  };
+
+  const statusChangeHandler = (value: string) => (
+    statusChanger(status?.task_status_id || '', currentTaskId || '', value)
+  );
 
   return (
     <>
@@ -33,7 +40,10 @@ const Details = () => {
         status?.name
         && (
           <DetailCategory name="Статус" type="details">
-            <TaskStatus defaultValue={status.name} />
+            <TaskStatus
+              defaultValue={status.name}
+              onChange={statusChangeHandler}
+            />
           </DetailCategory>
         )
       }
