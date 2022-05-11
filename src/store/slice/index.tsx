1041c2b49eceb6302fbea76d78f54963071df69f
Smoke52ru <Smoke52ru@ya.ru>
2022-05-12 01:08:44 +0300

    Фикс

--- src/store/slice/index.tsx
+++ src/store/slice/index.tsx
@@ -4,7 +4,6 @@ export * as TaskCompletedSlice from './task/taskCompleted';
 export * as TaskInboxSlice from './task/taskInbox';
 export * as UserSlice from './user';
 export * as UsersSlice from './users';
-export * as CommonActions from './task/commonActions';
 export * as CommonSlice from './task/commonActions';
 export * as TaskFormSlice from './task/taskForm';
 export * as TaskFilters from './task/taskFilters';
