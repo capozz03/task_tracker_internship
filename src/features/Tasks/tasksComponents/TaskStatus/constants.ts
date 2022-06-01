205535b371a08655ccb4caf51bd1601530d09d29
Smoke52ru <Smoke52ru@ya.ru>
2022-06-01 17:39:08 +0300

    Кнопка изменения статуса задачи: сменил цвета при наведении

--- src/features/Tasks/tasksComponents/TaskStatus/constants.ts
+++ src/features/Tasks/tasksComponents/TaskStatus/constants.ts
@@ -1,6 +1,7 @@
 type TaskStatusesType = {
   status: string;
   color: string;
+  hoverColor: string,
   taskStatusId: string;
 };
 
@@ -8,21 +9,25 @@ export const taskStatuses: TaskStatusesType[] = [
   {
     status: 'Создана',
     color: '#50B5FF',
+    hoverColor: '#1A72FF',
     taskStatusId: 'cbb7199e-cb25-4dce-bf4e-24a8a5e07ef2',
   },
   {
     status: 'В работе',
     color: '#3DD598',
+    hoverColor: '#82C43C',
     taskStatusId: '372d63ff-3ae3-4be2-a606-38940d7f8c8f',
   },
   {
     status: 'Выполнена',
     color: '#A461D8',
+    hoverColor: '#7E04DD',
     taskStatusId: '8536592a-7340-4e10-ac4b-a280652c9310',
   },
   {
     status: 'Не выполнена',
     color: '#FC5A5A',
+    hoverColor: '#DA1717',
     taskStatusId: '599f5d03-1ef0-4a5b-a18c-33a4f44c4610',
   },
 ];
