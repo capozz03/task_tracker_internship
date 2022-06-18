ce7e59d859dc6ef66582477088d9dace2a5223fc
Erichev Egor <ErichevEA@lad24.ru>
2022-06-18 23:37:54 +0300

    test

--- src/app/routing/AppRouter.tsx
+++ src/app/routing/AppRouter.tsx
@@ -4,49 +4,52 @@ import { Route, Routes } from 'react-router-dom';
 import { PrivatePageHOC } from './PrivatePageHOC';
 import { RoutingConfig } from './RoutingConfig';
 
-export const AppRouter = () => (
-  <Routes>
-    {RoutingConfig.map((el, i) => {
-      if (el.children?.length) {
-        return (
-          <Route key={i} path={el.path} element={el.element()}>
-            {el.children.map((child, childIndex) => {
-              if (child.index) {
+export const AppRouter = () => {
+  console.log('hi');
+  return (
+    <Routes>
+      {RoutingConfig.map((el, i) => {
+        if (el.children?.length) {
+          return (
+            <Route key={i} path={el.path} element={el.element()}>
+              {el.children.map((child, childIndex) => {
+                if (child.index) {
+                  if (child.private) {
+                    return (
+                      <Route
+                        key={childIndex}
+                        index
+                        element={<PrivatePageHOC>{child.element()}</PrivatePageHOC>}
+                      />
+                    );
+                  }
+                  return <Route key={childIndex} index element={child.element()} />;
+                }
                 if (child.private) {
                   return (
                     <Route
                       key={childIndex}
-                      index
+                      path={child.path}
                       element={<PrivatePageHOC>{child.element()}</PrivatePageHOC>}
                     />
                   );
                 }
-                return <Route key={childIndex} index element={child.element()} />;
-              }
-              if (child.private) {
-                return (
-                  <Route
-                    key={childIndex}
-                    path={child.path}
-                    element={<PrivatePageHOC>{child.element()}</PrivatePageHOC>}
-                  />
-                );
-              }
-              return <Route key={childIndex} path={child.path} element={child.element()} />;
-            })}
-          </Route>
-        );
-      }
-      if (el.private) {
-        return (
-          <Route
-            key={i}
-            path={el.path}
-            element={<PrivatePageHOC>{el.element()}</PrivatePageHOC>}
-          />
-        );
-      }
-      return <Route key={i} path={el.path} element={el.element()} />;
-    })}
-  </Routes>
-);
+                return <Route key={childIndex} path={child.path} element={child.element()} />;
+              })}
+            </Route>
+          );
+        }
+        if (el.private) {
+          return (
+            <Route
+              key={i}
+              path={el.path}
+              element={<PrivatePageHOC>{el.element()}</PrivatePageHOC>}
+            />
+          );
+        }
+        return <Route key={i} path={el.path} element={el.element()} />;
+      })}
+    </Routes>
+  );
+};
